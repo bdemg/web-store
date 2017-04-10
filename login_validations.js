2ed165bd48eeda044bc50b8815d522c731068d62
Jorge Cano <jacano.sosa@gmail.com>
2017-04-10 16:10:41 -0500

    integracion de validaciones de login con index

--- login_validations.js
+++ login_validations.js
@@ -1,10 +1,10 @@
 function validateLogin(){
 	
-	if(document.forma.username.value == ""){
+	if(document.forma.usuario.value == ""){
 		
 		alert("El campo de nombre de usuario está vacío, favor de llenarlo");
 		return false;
-	}else if(document.forma.pass.value ==""){
+	}else if(document.forma.contrasena.value ==""){
 		
 		alert("El campo de contraseña está vacío, favor de llenarlo");
 		return false
