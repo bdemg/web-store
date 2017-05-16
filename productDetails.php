28251ad1fa59a2f39bbe21e7838b117cadfc1433
Jorge Cano <jacano.sosa@gmail.com>
2017-05-16 08:04:58 -0500

    Cambio de precio implementado

--- productDetails.php
+++ productDetails.php
@@ -18,11 +18,48 @@ if ($_SESSION[$admin] != true){
 <head>
 	<title><?php echo $_REQUEST["name"]; ?></title>
     <link rel="stylesheet" href="css/generalStyle.css">
+	<script>
+	<!--
+	window.onload = function () {
+		
+		document.forma.onsubmit = function() {
+			if(document.forma.price.value==""){
+				alert("Es necesario introducir un precio para el producto");
+				document.forma.price.focus();
+				return false;
+			}
+			if(!isPositiveNumber(document.forma.price)){
+				return false;
+			}
+			
+			return true;
+		}
+		
+	function isPositiveNumber(field){
+
+		var isPositiveNumber = ( !isNaN(field.value) ) && ( Number(field.value) > 0 );
+		
+		if(!isPositiveNumber){
+			
+			alert("El precio introducido no puede ser procesado, favor de cambiarlo");
+			field.value = "";
+			field.focus();
+		}
+		
+		return isPositiveNumber;
+	}
+	//-->
+	</script>
 </head>
+
 <body style="text-align: center">
 <h1><?php echo $_REQUEST["name"]; ?></h1>
 <img src=<?php echo " \"".$_REQUEST["image"]."\" "; ?> width="150" height="150" border="1" alt="" name="pan0"><br/>
 <p><?php echo $_REQUEST["descripcion"]; ?></p>
-<p>$<?php echo $_REQUEST["price"]; ?> c/u</p>
+<form name="forma" action="updatePrice.php" method="post">
+<p>$<input type="text" name="price" size="5" value=<?php echo "\"".$_REQUEST["price"]."\""; ?>> c/u</p>
+<input type="hidden" name="name" value=<?php echo "\"".$_REQUEST["name"]."\""; ?>>
+<p><input type="submit" name="cambiarPrecio" value="Cambiar precio"></p>
+</form>
 </body>
 </html>
---
+++ updatePrice.php
@@ -0,0 +1,52 @@
+<?php 
+// * * * * 
+session_start();
+include("sessionVariables.php");
+if ($_SESSION[$is_logged] != true) {
+	 header("location: index.php?estado=4");
+	 exit();
+}
+if ($_SESSION[$admin] != true){
+	header("location: index.php?estado=6");
+	exit();
+}
+// * * * *
+
+if(isset($_REQUEST["price"])){
+	
+	include("variables.php");
+	include("funciones.php");
+	$conexion = mysqli_connect($servidor, $usuario, $contrasena, $basedatos);
+	if (!$conexion) {
+		die("Fallo: " . mysqli_connect_error());
+	}
+	
+	$precioF = mysqli_real_escape_string($conexion, $_REQUEST["price"]);
+	$precioF = htmlentities($precioF, ENT_QUOTES);
+	$precioF = strip_tags($precioF);
+	
+	$sentenciaSQL = "UPDATE productos SET price=".$precioF." WHERE name=\"".$_REQUEST["name"]."\"";
+	EjecutarSQL ($servidor, $usuario, $contrasena, $basedatos, $sentenciaSQL);
+
+	echo "<html>
+<head>
+	<title>Actualizar Precio</title>
+	<script>
+		alert(\"Precio actualizado.\");
+		window.close();
+	</script>
+</head>
+</html>";
+}else{
+	
+	echo "<html>
+<head>
+	<title>Actualizar Precio</title>
+	<script>
+		alert(\"El precio no pudo ser actualizado.\");
+		history.back();
+	</script>
+</head>
+</html>";
+}
+?>
